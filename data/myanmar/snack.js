360aec687cd54f5ba6e99557337286a242d29a99
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2025-11-07 09:12:44 +0630

    Delete data directory
    To Change New Dir

--- data/myanmar/snack.js
+++
@@ -1,7 +0,0 @@
-// myanmar/snack.js — data for myanmar -> snack category
-window.menusPartials = window.menusPartials || {};
-window.menusPartials['myanmar'] = window.menusPartials['myanmar'] || {};
-window.menusPartials['myanmar'].snack = [
-  { name: 'ထမင်းမုန့်', desc: 'အချိုတစ်မျိုး', price: '5000 MMK', img: 'https://via.placeholder.com/120x80' }
-  
-];
