93d16359156e37cca2799e99729cdac5551a2f81
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2025-11-07 09:09:07 +0630

    Delete The Prince directory
    Test Mistake

--- The Prince/data/international/vegetable.js
+++
@@ -1,6 +0,0 @@
-// international/snack.js — data for international -> snack category
-window.menusPartials = window.menusPartials || {};
-window.menusPartials['international'] = window.menusPartials['international'] || {};
-window.menusPartials['international'].vegetable = [
-  { name: 'Chocolate Cake', desc: 'Rich and moist.', price: '1200 MMK', img: 'https://via.placeholder.com/120x80' }
-];
