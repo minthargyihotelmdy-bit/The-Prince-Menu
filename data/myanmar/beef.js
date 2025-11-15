97213d77a176dae7f63563979926b220eb5638a0
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2025-11-15 11:46:23 +0630

    Update beef menu items for Myanmar

--- data/myanmar/beef.js
+++ data/myanmar/beef.js
@@ -2,19 +2,15 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['myanmar'] = window.menusPartials['myanmar'] || {};
 window.menusPartials['myanmar'].beef = [
-  { name: 'Heineken Bot', desc: '', price: '8500 MMK', img: '' },
-  { name: 'Heineken Can', desc: '', price: '5500 MMK', img: '' },
-  { name: 'ABC Bot', desc: '', price: '10000 MMK', img: '' },
-  { name: 'ABC Can', desc: '', price: '7800 MMK', img: '' },
-  { name: 'Tiger Bot', desc: '', price: '8000 MMK', img: '' },
-  { name: 'Tiger Can', desc: '', price: '6000 MMK', img: '' },
-  { name: 'Myanmar Bot', desc: '', price: '5500 MMK', img: '' },
-  { name: 'Chang Bot', desc: '', price: '5000 MMK', img: '' },
-  { name: 'SPY', desc: '', price: '14000 MMK', img: '' },
-  { name: 'Sprite', desc: '', price: '3900 MMK', img: '' },
-  { name: 'Shark', desc: '', price: '3200 MMK', img: '' },
-  { name: 'Sunkit Can', desc: '', price: '3000 MMK', img: '' },
-  { name: 'Coke', desc: '', price: '5200 MMK', img: '' },
-  { name: 'Lippo', desc: '', price: '3700 MMK', img: '' },
-  { name: 'Red Cow Cheese', desc: '', price: '4300 MMK', img: '' },
+  { name: 'အမဲသား တရုတ်နံနံ', desc: '', price: '24000 MMK', img: 'images/06-beef/AMTYNN.jpg' },
+  { name: 'အမဲသားပင်စိမ်း', desc: '', price: '24000 MMK', img: '' },
+  { name: 'အမဲသားနှပ်', desc: '', price: '24000 MMK', img: 'images/06-beef/AMN.jpg' },
+  { name: 'အမဲသားဆီပြန်', desc: '', price: '24000 MMK', img: '' },
+  { name: 'အမဲသားအာလူး', desc: '', price: '24000 MMK', img: '' },
+  { name: 'အမဲသားထောင်း', desc: 'Small-Large', price: '24000 MMK-30000 MMK', img: '' },
+  { name: 'အမဲသားတုံကြော်', desc: 'Small-Large', price: '24000 MMK-30000 MMK', img: '' },
+  { name: 'အမဲသားမျှင်ကြော်', desc: 'Small-Large', price: '24000 MMK-30000 MMK', img: 'images/06-beef/AMAMK.jpg' },
+  { name: 'အမဲသားစွတ်ပြုတ်', desc: '', price: '24000 MMK', img: 'images/06-beef/AMTSP.jpg' },
+
+ 
 ];
