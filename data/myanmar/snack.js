504bd5d7189d349ca84cdbbe6aa6c88e86c27c3b
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2025-11-17 16:24:38 +0630

    Update snack.js

--- data/myanmar/snack.js
+++ data/myanmar/snack.js
@@ -8,6 +8,11 @@ window.menusPartials['myanmar'].snack = [
   { name: 'ကြက်ကောင်လုံးကြော်', desc: '', price: '40000 MMK', img: '' },
   { name: 'အော်ဂင်နစ်ကြက်ကြော်', desc: '', price: '40000 MMK', img: '' },
   { name: 'အကင်းစုံ', desc: '', price: '29000 MMK', img: '' },
-  
+  { name: 'အသားလုံးကြော် (ကြက်)', desc: 'Small-large', price: '16000 MMK-20000 MMK', img: '' },
+  { name: 'အသားလုံးကြော် (ဝက်)', desc: 'Small-large', price: '16000 MMK-20000 MMK', img: '' },
+  { name: 'ပင်လယ်စာကြွပ်ကြော်', desc: '', price: '33000 MMK', img: '' },
+  { name: 'ငါးရံခြောက်ဆီဆမ်း', desc: '', price: '30000 MMK', img: '' },
+  { name: 'ဝက်အူချောင်းကြော်', desc: 'Small-large', price: '16000 MMK-20000 MMK', img: '' },
 
 ];
+
