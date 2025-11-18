5a01b3b2b8f907ad7241b06cada21545e6824d66
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2025-11-18 10:41:20 +0630

    Add beer menu data for Myanmar

--- data/international/beer.js
+++ data/international/beer.js
@@ -1,7 +1,21 @@
-// international/chicken.js — data for international -> chicken category
+// myanmar/chicken.js — data for myanmar -> chicken category
 window.menusPartials = window.menusPartials || {};
-window.menusPartials['international'] = window.menusPartials['international'] || {};
-window.menusPartials['international'].beer = [
-  { name: 'Chicken Rice', desc: 'Served with spicy sauce.', price: '1200 MMK', img: '' },
-  { name: 'Beef Steak', desc: 'Cooked to your preference.', price: '1200 MMK', img: '' }
+window.menusPartials['myanmar'] = window.menusPartials['myanmar'] || {};
+window.menusPartials['myanmar'].beer = [
+  { name: 'Heineken Bot', desc: '', price: '8500 MMK', img: '' },
+  { name: 'Heineken Can', desc: '', price: '5500 MMK', img: '' },
+  { name: 'ABC bot', desc: '', price: '10000 MMK', img: '' },
+  { name: 'ABC Can', desc: '', price: '7800 MMK', img: '' },
+  { name: 'Tiger Bot', desc: '', price: '8000 MMK', img: '' },
+  { name: 'Tiger Can', desc: '', price: '6000 MMK', img: '' },
+  { name: 'Myanmar Bot', desc: '', price: '5500 MMK', img: '' },
+  { name: 'Chang Bot', desc: '', price: '5000 MMK', img: '' },
+  { name: 'SPY', desc: '', price: '14000 MMK', img: '' },
+  { name: 'Sprite', desc: '', price: '3900 MMK', img: '' },
+  { name: 'Shark', desc: '', price: '3200 MMK', img: '' },
+  { name: 'Sunkit can', desc: '', price: '3000 MMK', img: '' },
+  { name: 'Coke', desc: '', price: '5200 MMK', img: '' },
+  { name: 'Lippo', desc: '', price: '3700 MMK', img: '' },
+  { name: 'Red Cow Chese', desc: '', price: '4300 MMK', img: '' },
+
 ];
