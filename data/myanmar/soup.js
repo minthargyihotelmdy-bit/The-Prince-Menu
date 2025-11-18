d5f7169fcbd25a3d588f99e6b52daf17d1e99ae2
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2025-11-18 10:54:53 +0630

    Refactor soup data structure in soup.js

--- data/myanmar/soup.js
+++ data/myanmar/soup.js
@@ -8,16 +8,17 @@ window.menusPartials['myanmar'].soup = [
   { name: 'နံရိုးကြံမဆိုင်ဟင်းရည်', desc: 'Small-Large', price: '29000 MMK-36000 MMK', img: '' },
   { name: 'ကြက်ဂွေးတောက်ဟင်းရည်', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
   { name: 'ကြက်ကာလသားဟင်းရည်(ဗူးသီး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
-  { name: '(၁၂)မျိုးဟင်းချို (ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
-  { name: '(၁၂)မျိုးဟင်းချို(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
-  { name: 'တုန့်ယန်းဟင်းချို(ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: 'images/09-soup/TYHY.jpg' },
-  { name: 'တုန့်ယန်းဟင်းချို(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: 'images/09-soup/TYHY.jpg' },
-  { name: 'ယိုးဒယားဟင်းချို(ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
-  { name: 'ယိုးဒယားဟင်းချို(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
-  { name: 'ကြံမဆိုင်ဟင်းချို(ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
-  { name: 'ကြံမဆိုင်ဟင်းချို(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
+  { name: '(၁၂)မျိုးဟင်းရည် (ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
+  { name: '(၁၂)မျိုးဟင်းရည်(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
+  { name: 'တုန့်ယန်းဟင်းရည်(ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: 'images/09-soup/TYHY.jpg' },
+  { name: 'တုန့်ယန်းဟင်းရည်(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: 'images/09-soup/TYHY.jpg' },
+  { name: 'ယိုးဒယားဟင်းရည်(ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
+  { name: 'ယိုးဒယားဟင်းရည်(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
+  { name: 'ကြံမဆိုင်ဟင်းရည်(ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
+  { name: 'ကြံမဆိုင်ဟင်းရည်(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
   { name: 'တောကြက်ပေါင်းဟင်းရည်', desc: 'ကြက်လယ်သမား', price: '38000 MMK', img: '' },
   { name: 'ငါးခူးလယ်သမားဟင်းရည်', desc: '', price: '15000 MMK', img: '' },
    
 ];
+
 
