1d21aeb47cf668eeaadd6e8debb7e7da1776e3bf
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2025-11-18 10:53:39 +0630

    Update soup.js

--- data/myanmar/soup.js
+++ data/myanmar/soup.js
@@ -3,10 +3,10 @@ window.menusPartials = window.menusPartials || {};
 window.menusPartials['myanmar'] = window.menusPartials['myanmar'] || {};
 window.menusPartials['myanmar'].soup = [
   { name: 'သီးရွက်စုံဟင်းရည်', desc: 'Small-Large', price: '10500 MMK-13500 MMK', img: '' },
-  { name: 'ငါးခေါင်းဟင်းချို', desc: 'Small-Large', price: '12000 MMK-16000 MMK', img: 'images/09-soup/NGHY.jpg' },
-  { name: 'ငုံးဥငါးဖယ်လုံးဟင်းချို', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
-  { name: 'နံရိုးကြံမဆိုင်ဟင်းချို', desc: 'Small-Large', price: '29000 MMK-36000 MMK', img: '' },
-  { name: 'ကြက်ဂွေးတောက်ဟင်းချို', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
+  { name: 'ငါးခေါင်းဟင်းရည်', desc: 'Small-Large', price: '12000 MMK-16000 MMK', img: 'images/09-soup/NGHY.jpg' },
+  { name: 'ငုံးဥငါးဖယ်လုံးဟင်းရည်', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
+  { name: 'နံရိုးကြံမဆိုင်ဟင်းရည်', desc: 'Small-Large', price: '29000 MMK-36000 MMK', img: '' },
+  { name: 'ကြက်ဂွေးတောက်ဟင်းရည်', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
   { name: 'ကြက်ကာလသားဟင်းရည်(ဗူးသီး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
   { name: '(၁၂)မျိုးဟင်းချို (ကြက်/ဝက်/ငါး)', desc: 'Small-Large', price: '16000 MMK-20000 MMK', img: '' },
   { name: '(၁၂)မျိုးဟင်းချို(ပုဇွန်/အဏ္ဏဝါ)', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
@@ -20,3 +20,4 @@ window.menusPartials['myanmar'].soup = [
   { name: 'ငါးခူးလယ်သမားဟင်းရည်', desc: '', price: '15000 MMK', img: '' },
    
 ];
+
