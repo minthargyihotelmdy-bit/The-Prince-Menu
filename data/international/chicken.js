321e4961593a8c1f4d55189947b8550058701123
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2025-11-25 16:12:39 +0630

    Update chicken.js

--- data/international/chicken.js
+++ data/international/chicken.js
@@ -2,14 +2,14 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['international'] = window.menusPartials['international'] || {};
 window.menusPartials['international'].chicken = [
-  { name: 'Hot&SweetChicken', desc: 'Small-Large', price: '16000 MMK', img: '' },
-  { name: 'Fried Chicken with Banboo Shoot', desc: 'Small-Large', price: '16000 MMK', img: '' },
-  { name: 'Fired Chicken With Basil Leaf', desc: 'Small-Large', price: '16000 MMK', img: '' },
-  { name: 'Chicken Kabad', desc: 'Small-Large', price: '16000 MMK', img: '' },
-  { name: 'Fired Finger Chicken ', desc: 'Small-Large', price: '16000 MMK', img: '' },
-  { name: 'Chicken Omelette', desc: '', price: '16000 MMK', img: '' },
-  { name: 'Fired Chicken with Tomyum Paste', desc: 'Small-Large', price: '16000 MMK', img: '' },
-  { name: 'Deep Fired Chicken Wing', desc: 'Small-Large', price: '16000 MMK', img: '' },
-  { name: 'Fired Chicken ', desc: 'Small-Large', price: '16000 MMK', img: '' },
-  { name: 'Fired Chicken Kachin Style', desc: 'Small-Large', price: '16000 MMK', img: '' },
+  { name: 'Hot&SweetChicken', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Fried Chicken with Banboo Shoot', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Fired Chicken With Basil Leaf', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Chicken Kabad', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Fired Finger Chicken ', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Chicken Omelette', desc: '', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Fired Chicken with Tomyum Paste', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Deep Fired Chicken Wing', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Fired Chicken ', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
+  { name: 'Fired Chicken Kachin Style', desc: 'Small-Large', price: '16000 MMK- 20000 MMK', img: '' },
 ];
