cc8d9b580bd374ecf9079d33b2dfaf85b6537a81
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2026-01-25 11:44:18 +0630

    Adjust mutton prices in myanmar menu
    Updated prices for mutton dishes in Myanmar category.

--- data/myanmar/mutton.js
+++ data/myanmar/mutton.js
@@ -2,13 +2,14 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['myanmar'] = window.menusPartials['myanmar'] || {};
 window.menusPartials['myanmar'].mutton = [
-  { name: 'ဆိတ်မျှင်ကြော်', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: 'images/05-mutton/SAMK.jpg' },
-  { name: 'ဆိတ်မျှင်ခြောက်စပ်', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: 'images/05-mutton/SAMCS.jpg' },
-  { name: 'ဆိတ်သားအတုံးကြော်', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
-  { name: 'ဆိတ်သားခဲပတ်', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
-  { name: 'ဆိတ်ပေါင်းအစပ်ချက်', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: 'images/05-mutton/SPCS.jpg' },
-  { name: 'ဆိတ်ပေါင်းမဆလာ', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
+  { name: 'ဆိတ်မျှင်ကြော်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: 'images/05-mutton/SAMK.jpg' },
+  { name: 'ဆိတ်မျှင်ခြောက်စပ်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: 'images/05-mutton/SAMCS.jpg' },
+  { name: 'ဆိတ်သားအတုံးကြော်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
+  { name: 'ဆိတ်သားခဲပတ်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
+  { name: 'ဆိတ်ပေါင်းအစပ်ချက်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: 'images/05-mutton/SPCS.jpg' },
+  { name: 'ဆိတ်ပေါင်းမဆလာ', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
   { name: 'ဆိတ်သားခြောက်ဆီဆမ်း', desc: '', price: '30000 MMK', img: 'images/05-mutton/STCSS.jpg' },
-  { name: 'ဆိတ်ထောင်းလက်ဖက်', desc: 'Small-Large', price: '23000 MMK-31000 MMK', img: 'images/05-mutton/SLPH.jpg' }
+  { name: 'ဆိတ်ထောင်းလက်ဖက်', desc: 'Small-Large', price: '28000 MMK-37000 MMK', img: 'images/05-mutton/SLPH.jpg' }
   
 ];
+
