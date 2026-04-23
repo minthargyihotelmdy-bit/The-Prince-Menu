063445d24be6bcbf60dd466efb85e4c2e8feed0f
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2026-04-23 19:49:57 +0630

    Update prices for Myanmar food items
    Updated food prices for various Myanmar dishes.

--- data/myanmar/myanmar_food.js
+++ data/myanmar/myanmar_food.js
@@ -2,14 +2,14 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['myanmar'] = window.menusPartials['myanmar'] || {};
 window.menusPartials['myanmar'].myanmar_food = [
-{ name: 'ကြက်သားအာလူး', desc: '', price: ' 16000 MMK', img: '' },
-{ name: 'ကြက်သားဆီပြန်', desc: '', price: '16000 MMK', img: '' },
-{ name: 'ကြက်သားရေချိုချက်', desc: '', price: '16000 MMK', img: '' },
-{ name: 'ဝက်သားအာလူး', desc: '', price: '16000 MMK', img: '' },
-{ name: 'ဝက်သားနီချက်', desc: '', price: '16000 MMK', img: '' },
-{ name: 'ဝက်ပုန်းရည်', desc: '', price: '16000 MMK', img: 'images/11-13/MYS_WPY.jpg' },
-{ name: 'ငါးဆီပြန်', desc: '', price: '16000 MMK', img: '' },
-{ name: 'ငါးသေတ္တာဆီပြန်', desc: '', price: '13000 MMK', img: '' },
-{ name: 'ဆိတ်သားအာလူး', desc: '', price: '20000 MMK', img: '' },
-{ name: 'ဆိတ်သားဆီပြန်', desc: '', price: '20000 MMK', img: '' },
+{ name: 'ကြက်သားအာလူး', desc: '', price: ' 20000 MMK', img: '' },
+{ name: 'ကြက်သားဆီပြန်', desc: '', price: '20000 MMK', img: '' },
+{ name: 'ကြက်သားရေချိုချက်', desc: '', price: '20000 MMK', img: '' },
+{ name: 'ဝက်သားအာလူး', desc: '', price: '20000 MMK', img: '' },
+{ name: 'ဝက်သားနီချက်', desc: '', price: '20000 MMK', img: '' },
+{ name: 'ဝက်ပုန်းရည်', desc: '', price: '20000 MMK', img: 'images/11-13/MYS_WPY.jpg' },
+{ name: 'ငါးဆီပြန်', desc: '', price: '20000 MMK', img: '' },
+{ name: 'ငါးသေတ္တာဆီပြန်', desc: '', price: '16000 MMK', img: '' },
+{ name: 'ဆိတ်သားအာလူး', desc: '', price: '30000 MMK', img: '' },
+{ name: 'ဆိတ်သားဆီပြန်', desc: '', price: '30000 MMK', img: '' },
 ];
