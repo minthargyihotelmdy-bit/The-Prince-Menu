2a9c4590ade29e976144088b4cb9ec23ab90024b
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2026-04-23 19:46:48 +0630

    Revise mutton category data with new prices
    Updated mutton menu prices and descriptions.

--- data/myanmar/mutton.js
+++ data/myanmar/mutton.js
@@ -2,14 +2,14 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['myanmar'] = window.menusPartials['myanmar'] || {};
 window.menusPartials['myanmar'].mutton = [
-  { name: 'ဆိတ်မျှင်ကြော်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: 'images/05-mutton/SAMK.jpg' },
-  { name: 'ဆိတ်မျှင်ခြောက်စပ်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: 'images/05-mutton/SAMCS.jpg' },
-  { name: 'ဆိတ်သားအတုံးကြော်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
-  { name: 'ဆိတ်သားခဲပတ်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
-  { name: 'ဆိတ်ပေါင်းအစပ်ချက်', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: 'images/05-mutton/SPCS.jpg' },
-  { name: 'ဆိတ်ပေါင်းမဆလာ', desc: 'Small-Large', price: '25000 MMK-35000 MMK', img: '' },
-  { name: 'ဆိတ်သားခြောက်ဆီဆမ်း', desc: '', price: '30000 MMK', img: 'images/05-mutton/STCSS.jpg' },
-  { name: 'ဆိတ်ထောင်းလက်ဖက်', desc: 'Small-Large', price: '28000 MMK-37000 MMK', img: 'images/05-mutton/SLPH.jpg' }
+  { name: 'ဆိတ်မျှင်ကြော်', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: 'images/05-mutton/SAMK.jpg' },
+  { name: 'ဆိတ်မျှင်ခြောက်စပ်', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: 'images/05-mutton/SAMCS.jpg' },
+  { name: 'ဆိတ်သားအတုံးကြော်', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'ဆိတ်သားခဲပတ်', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'ဆိတ်ပေါင်းအစပ်ချက်', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: 'images/05-mutton/SPCS.jpg' },
+  { name: 'ဆိတ်ပေါင်းမဆလာ', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'ဆိတ်သားခြောက်ဆီဆမ်း', desc: '', price: '36000 MMK', img: 'images/05-mutton/STCSS.jpg' },
+  { name: 'ဆိတ်ထောင်းလက်ဖက်', desc: 'Small-Large', price: '34000 MMK-45000 MMK', img: 'images/05-mutton/SLPH.jpg' }
   
 ];
 
