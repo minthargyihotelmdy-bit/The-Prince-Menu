7649e00fd04355cb747e6a2b89ede59325b3c067
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2026-06-12 21:59:45 +0630

    Update snack prices in myanmar/snack.js
    Updated snack prices and added new price ranges for various items.

--- data/myanmar/snack.js
+++ data/myanmar/snack.js
@@ -2,21 +2,21 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['myanmar'] = window.menusPartials['myanmar'] || {};
 window.menusPartials['myanmar'].snack = [
-  { name: 'ဟာဝိုင်ယီအစုံကြော်', desc: '', price: '50000 MMK', img: 'images/Snack/HWEI-min.jpg' },
-  { name: 'လေးမျိုးအခြောက်ကြော်', desc: '', price: '48000 MMK', img: 'images/Snack/4K-min.jpg' },
-  { name: 'ဝက်အူနုကြော်', desc: '', price: '20000 MMK', img: 'images/Snack/PON-min.jpg' }, 
-  { name: 'ကြက်ကောင်လုံးကြော်', desc: '', price: '40000 MMK', img: '' },
-  { name: 'အော်ဂင်နစ်ကြက်ကြော်', desc: '', price: '40000 MMK', img: '' },
-  { name: 'အကင်စုံ', desc: '', price: '29000 MMK', img: '' },
-  { name: 'အသားလုံးကြော် (ကြက်)', desc: 'Small-large', price: '16000 MMK-20000 MMK', img: '' },
-  { name: 'အသားလုံးကြော် (ဝက်)', desc: 'Small-large', price: '16000 MMK-20000 MMK', img: '' },
-  { name: 'ပင်လယ်စာကြွပ်ကြော်', desc: '', price: '32000 MMK', img: '' },
-  { name: 'ငါးရံခြောက်ဆီဆမ်း', desc: '', price: '30000 MMK', img: '' },
-  { name: 'ဝက်အူချောင်းကြော်', desc: 'Small-large', price: '16000 MMK-20000 MMK', img: '' },
-  { name: 'နံကင်ကြော်', desc: 'Small-large', price: '36000 MMK-45000 MMK', img: '' },
-  { name: 'ဝက်နံရိုးကြော်', desc: 'Small-large', price: '29000 MMK-36000 MMK', img: '' },
-  { name: 'ပုဇွန်တန်ပူရာ', desc: 'Small-large', price: '25000 MMK-30000 MMK', img: '' },
-  { name: 'ပုဇွန်အာလူးလိပ်', desc: 'Small-large', price: '28000 MMK-40000 MMK', img: '' },
+  { name: 'ဟာဝိုင်ယီအစုံကြော်', desc: '', price: '60000 MMK', img: 'images/Snack/HWEI-min.jpg' },
+  { name: 'လေးမျိုးအခြောက်ကြော်', desc: '', price: '58000 MMK', img: 'images/Snack/4K-min.jpg' },
+  { name: 'ဝက်အူနုကြော်', desc: '', price: '25000 MMK', img: 'images/Snack/PON-min.jpg' }, 
+  { name: 'ကြက်ကောင်လုံးကြော်', desc: '', price: '48000 MMK', img: '' },
+  { name: 'အော်ဂင်နစ်ကြက်ကြော်', desc: '', price: '48000 MMK', img: '' },
+  { name: 'အကင်စုံ', desc: '', price: '35000 MMK', img: '' },
+  { name: 'အသားလုံးကြော် (ကြက်)', desc: 'Small-large', price: '20000 MMK-24000 MMK', img: '' },
+  { name: 'အသားလုံးကြော် (ဝက်)', desc: 'Small-large', price: '20000 MMK-24000 MMK', img: '' },
+  { name: 'ပင်လယ်စာကြွပ်ကြော်', desc: '', price: '39000 MMK', img: '' },
+  { name: 'ငါးရံခြောက်ဆီဆမ်း', desc: '', price: '36000 MMK', img: '' },
+  { name: 'ဝက်အူချောင်းကြော်', desc: 'Small-large', price: '20000 MMK-24000 MMK', img: '' },
+  { name: 'နံကင်ကြော်', desc: 'Small-large', price: '44000 MMK-54000 MMK', img: '' },
+  { name: 'ဝက်နံရိုးကြော်', desc: 'Small-large', price: '35000 MMK-44000 MMK', img: '' },
+  { name: 'ပုဇွန်တန်ပူရာ', desc: 'Small-large', price: '30000 MMK-36000 MMK', img: '' },
+  { name: 'ပုဇွန်အာလူးလိပ်', desc: 'Small-large', price: '34000 MMK-48000 MMK', img: '' },
 
 
 ];
