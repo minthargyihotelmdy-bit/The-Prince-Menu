3ce3679a0e45f39629777f05730b67b27e3d54b2
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2026-06-25 19:59:12 +0630

    Change beef menu item prices to 29000 MMK
    Updated prices for beef menu items in international category.

--- data/international/beef.js
+++ data/international/beef.js
@@ -2,14 +2,14 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['international'] = window.menusPartials['international'] || {};
 window.menusPartials['international'].beef = [
-  { name: 'Beef and Chinese celery', desc: '', price: '24000 MMK', img: '' },
-  { name: 'Beef and basil', desc: '', price: '24000 MMK', img: '' },
-  { name: 'Beef stew', desc: '', price: '24000 MMK', img: '' },
-  { name: 'Beef Curry', desc: '', price: '24000 MMK', img: '' },
-  { name: 'Beff Potato Curry', desc: '', price: '24000 MMK', img: '' },
-  { name: 'Ground beef', desc: 'Small-Large', price: '24000 MMK-30000 MMK', img: '' },
-  { name: 'Fried beef stew', desc: 'Small-Large', price: '24000 MMK-30000 MMK', img: '' },
-  { name: 'Fried beef skewers', desc: 'Small-Large', price: '24000 MMK-30000 MMK', img: '' },
-  { name: 'Soup ', desc: '', price: '24000 MMK', img: '' },
+  { name: 'Beef and Chinese celery', desc: '', price: '29000 MMK', img: '' },
+  { name: 'Beef and basil', desc: '', price: '29000 MMK', img: '' },
+  { name: 'Beef stew', desc: '', price: '29000 MMK', img: '' },
+  { name: 'Beef Curry', desc: '', price: '29000 MMK', img: '' },
+  { name: 'Beff Potato Curry', desc: '', price: '29000 MMK', img: '' },
+  { name: 'Ground beef', desc: 'Small-Large', price: '29000 MMK-36000 MMK', img: '' },
+  { name: 'Fried beef stew', desc: 'Small-Large', price: '29000 MMK-36000 MMK', img: '' },
+  { name: 'Fried beef skewers', desc: 'Small-Large', price: '29000 MMK-36000 MMK', img: '' },
+  { name: 'Soup ', desc: '', price: '29000 MMK', img: '' },
 
 ];
