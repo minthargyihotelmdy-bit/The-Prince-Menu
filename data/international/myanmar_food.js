7b5695a77a2b5cf68d1c5b19e883d4b7fb808159
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2026-06-26 21:14:00 +0630

    Adjust prices for Myanmar food menu items
    Updated prices for Myanmar food items in the menu.

--- data/international/myanmar_food.js
+++ data/international/myanmar_food.js
@@ -2,14 +2,14 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['international'] = window.menusPartials['international'] || {};
 window.menusPartials['international'].myanmar_food = [
-{ name: 'Braised Chicken with Potatoes', desc: '', price: ' 16000 MMK', img: '' },
-{ name: 'Stewed Chicken Oily', desc: '', price: '16000 MMK', img: '' },
-{ name: 'Stewed Chicken', desc: '', price: '16000 MMK', img: '' },
-{ name: 'Braised Pork with Potatoes', desc: '', price: '16000 MMK', img: '' },
-{ name: 'Mandalay Braised Pork Coloured Red', desc: '', price: '16000 MMK', img: '' },
-{ name: 'Stewed Pork with Preserved Bean Paste', desc: '', price: '16000 MMK', img: '' },
-{ name: 'Stewed Fish Oily', desc: '', price: '16000 MMK', img: '' },
-{ name: 'Stewed Sardine Oily', desc: '', price: '13000', img: '' },
-{ name: 'Braised Mutton with Potatoes', desc: '', price: '20000', img: '' },
-{ name: 'Stewed Mutton Oily', desc: '', price: '20000', img: '' },
+{ name: 'Braised Chicken with Potatoes', desc: '', price: ' 20000 MMK', img: '' },
+{ name: 'Stewed Chicken Oily', desc: '', price: '20000 MMK', img: '' },
+{ name: 'Stewed Chicken', desc: '', price: '20000 MMK', img: '' },
+{ name: 'Braised Pork with Potatoes', desc: '', price: '20000 MMK', img: '' },
+{ name: 'Mandalay Braised Pork Coloured Red', desc: '', price: '20000 MMK', img: '' },
+{ name: 'Stewed Pork with Preserved Bean Paste', desc: '', price: '20000 MMK', img: '' },
+{ name: 'Stewed Fish Oily', desc: '', price: '20000 MMK', img: '' },
+{ name: 'Stewed Sardine Oily', desc: '', price: '16000', img: '' },
+{ name: 'Braised Mutton with Potatoes', desc: '', price: '30000', img: '' },
+{ name: 'Stewed Mutton Oily', desc: '', price: '30000', img: '' },
 ];
