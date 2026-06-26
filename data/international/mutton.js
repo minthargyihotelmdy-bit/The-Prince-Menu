a32c44f2bd41b5c44c2626e31c15f2e8e9eeb8a2
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2026-06-26 21:11:02 +0630

    Revise mutton prices in international menu
    Updated mutton menu items with new prices.

--- data/international/mutton.js
+++ data/international/mutton.js
@@ -2,12 +2,12 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['international'] = window.menusPartials['international'] || {};
 window.menusPartials['international'].mutton = [
-  { name: 'Filament Mutton', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
-  { name: 'Filament Mutton with Spicy ', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
-  { name: 'Deep Fired Mutton ', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
-  { name: 'Mutton Kabad', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
-  { name: 'Steamed Mutton with Spicy Paste ', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
-  { name: 'Steamed Mutton with Curry Pownder', desc: 'Small-Large', price: '20000 MMK-28000 MMK', img: '' },
-  { name: 'ဆိတ်သားခြောက်ဆီဆမ်း', desc: '', price: '30000 MMK', img: '' },
-  { name: 'ဆိတ်ထောင်းလက်ဖက်', desc: 'Small-Large', price: '23000 MMK-31000 MMK', img: '' }
+  { name: 'Filament Mutton', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'Filament Mutton with Spicy ', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'Deep Fired Mutton ', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'Mutton Kabad', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'Steamed Mutton with Spicy Paste ', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'Steamed Mutton with Curry Pownder', desc: 'Small-Large', price: '30000 MMK-42000 MMK', img: '' },
+  { name: 'ဆိတ်သားခြောက်ဆီဆမ်း', desc: '', price: '36000 MMK', img: '' },
+  { name: 'ဆိတ်ထောင်းလက်ဖက်', desc: 'Small-Large', price: '34000 MMK-45000 MMK', img: '' }
 ];
