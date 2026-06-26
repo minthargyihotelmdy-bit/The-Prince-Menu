effe243896fec5161c0c26782d3925a385f0aee7
minthargyihotelmdy-bit <minthargyi.hotel.mdy@gmail.com>
2026-06-26 21:08:58 +0630

    Modify prices for light food items
    Updated prices for light food items in the international menu.

--- data/international/light_food.js
+++ data/international/light_food.js
@@ -2,15 +2,15 @@
 window.menusPartials = window.menusPartials || {};
 window.menusPartials['international'] = window.menusPartials['international'] || {};
 window.menusPartials['international'].light_food = [
-  { name: 'Toast Butter & Jam', desc: '', price: '3500 MMK', img: '' },
-  { name: 'Bombay Toast', desc: '', price: '5500 MMK', img: '' },
-  { name: 'Hamburger ', desc: '(chicken/Pork)', price: '10000 MMK', img: '' },
-  { name: 'Sandwich(Chicken/Cheese', desc: 'Small-Large', price: '10000 MMK- 16000 MMK', img: '' },
-  { name: 'Sandwich(Tuna Sardine)', desc: '', price: '20000 MMK', img: '' },
-  { name: 'Fried Cashew-nut', desc: '', price: '16000 MMK', img: '' },
-  { name: 'Fired finger Potatoes', desc: 'Small-Large', price: '9500 MMK- 12000 MMK', img: '' },
-  { name: 'Fired Potatoes Chip', desc: 'Small-Large', price: '9500 MMK- 12000 MMK', img: '' },
-  { name: 'Myanmar Tea-leaf Salad ', desc: '', price: '11000 MMK', img: '' },
-  { name: 'Myanamr Ginger Salad', desc: '', price: '11000 MMK', img: '' },
-  { name: 'Tomatoe Salad', desc: '', price: '10000 MMK', img: '' }, 
+  { name: 'Toast Butter & Jam', desc: '', price: '4500 MMK', img: '' },
+  { name: 'Bombay Toast', desc: '', price: '7000 MMK', img: '' },
+  { name: 'Hamburger ', desc: '(chicken/Pork)', price: '15000 MMK', img: '' },
+  { name: 'Sandwich(Chicken/Cheese', desc: 'Small-Large', price: '15000 MMK- 20000 MMK', img: '' },
+  { name: 'Sandwich(Tuna Sardine)', desc: '', price: '25000 MMK', img: '' },
+  { name: 'Fried Cashew-nut', desc: '', price: '20000 MMK', img: '' },
+  { name: 'Fired finger Potatoes', desc: 'Small-Large', price: '12000 MMK- 15000 MMK', img: '' },
+  { name: 'Fired Potatoes Chip', desc: 'Small-Large', price: '12000 MMK- 15000 MMK', img: '' },
+  { name: 'Myanmar Tea-leaf Salad ', desc: '', price: '15000 MMK', img: '' },
+  { name: 'Myanamr Ginger Salad', desc: '', price: '13000 MMK', img: '' },
+  { name: 'Tomatoe Salad', desc: '', price: '13000 MMK', img: '' }, 
 ];
